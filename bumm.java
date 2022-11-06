76d928f20738a120acb80689450ac71acb0d6899
Vntvxyz <117195555+Vntvxyz@users.noreply.github.com>
2022-11-06 16:51:32 +0700

    Update bumm.java

--- bumm.java
+++ bumm.java
@@ -66,7 +66,7 @@ public class Dos implements Runnable {
             dos.sslCheckConnection(url);
         }
 
-        System.out.println("Setting DDoS By: Shadow Tak");
+        System.out.println("Setting DDoS By: Thành Vinh");
 
         System.out.print("Thread: ");
         String amount = in.nextLine();
